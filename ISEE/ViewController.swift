86291183b7790ff20e5ccea4634c6d7ee6639d24
李博闻 <libowen@libowendemacbook-pro.local>
2016-10-21 22:40:52 +0800

    Initial Commit

---
+++ ISEE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ISEE
+//
+//  Created by 李博闻 on 16/10/21.
+//  Copyright © 2016年 小艺艺. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ISEETests/ISEETests.swift
@@ -0,0 +1,36 @@
+//
+//  ISEETests.swift
+//  ISEETests
+//
+//  Created by 李博闻 on 16/10/21.
+//  Copyright © 2016年 小艺艺. All rights reserved.
+//
+
+import XCTest
+@testable import ISEE
+
+class ISEETests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
